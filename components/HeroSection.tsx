f007cde2a810ea5810608181652aeca7c246e1d4
SyedAlhusain <108646522+SyedAlhusain@users.noreply.github.com>
2024-02-22 01:14:34 -0500

    Update HeroSection.tsx

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,7 +1,8 @@
-import React from "react";
-import Image from "next/image";
-import { Link } from "react-scroll/modules";
-import { HiArrowDown } from "react-icons/hi";
+"use client" // this is a client component
+import React from "react"
+import Image from "next/image"
+import { Link } from "react-scroll/modules"
+import { HiArrowDown } from "react-icons/hi"
 
 const HeroSection = () => {
   return (
@@ -9,7 +10,7 @@ const HeroSection = () => {
       <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
         <div className="md:mt-2 md:w-1/2">
           <Image
-            src="/Syed_Husain.jpeg"
+            src="/headshot.png"
             alt=""
             width={325}
             height={325}
@@ -17,13 +18,13 @@ const HeroSection = () => {
           />
         </div>
         <div className="md:mt-2 md:w-3/5">
-          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&#39;m Syed Husain!</h1>
+          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&#39;m Hosna!</h1>
           <p className="text-lg mt-4 mb-6 md:text-2xl">
             I&#39;m a{" "}
             <span className="font-semibold text-teal-600">
               Software Engineer{" "}
             </span>
-            based in Jessup, MD. Working towards creating software that
+            based in Los Angeles, CA. Working towards creating software that
             makes life easier and more meaningful.
           </p>
           <Link
@@ -40,7 +41,6 @@ const HeroSection = () => {
         </div>
       </div>
       <div className="flex flex-row items-center text-center justify-center ">
-        {/* Wrap the HiArrowDown component with the Link component */}
         <Link
           to="about"
           activeClass="active"
@@ -53,8 +53,7 @@ const HeroSection = () => {
         </Link>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default HeroSection;
-
+export default HeroSection
