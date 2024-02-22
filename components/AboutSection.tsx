cfe2cdb4da1f37abba04daabea735b2a06cc183e
Syed Husain <syedhusain@syeds-mbp-2.mynetworksettings.com>
2024-02-21 23:47:42 -0500

    first commit

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -1,21 +1,114 @@
-import React from "react"
-import Image from "next/image"
+// import React from "react"
+// import Image from "next/image"
 
-const skills = [
-  { skill: "HTML" },
-  { skill: "CSS" },
-  { skill: "JavaScript" },
-  { skill: "TypeScript" },
-  { skill: "Python" },
-  { skill: "React" },
-  { skill: "Next.js" },
-  { skill: "Tailwind CSS" },
-  { skill: "Git" },
-  { skill: "GitHub" },
-  { skill: "Jupyter Notebooks" },
-]
+// const skills = [
+//   { skill: "HTML" },
+//   { skill: "CSS" },
+//   { skill: "JavaScript" },
+//   { skill: "TypeScript" },
+//   { skill: "Python" },
+//   { skill: "React" },
+//   { skill: "Next.js" },
+//   { skill: "Tailwind CSS" },
+//   { skill: "Git" },
+//   { skill: "GitHub" },
+//   { skill: "Jupyter Notebooks" },
+// ]
+
+// const AboutSection = () => {
+//   return (
+//     <section id="about">
+//       <div className="my-12 pb-12 md:pt-16 md:pb-48">
+//         <h1 className="text-center font-bold text-4xl">
+//           About Me
+//           <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+//         </h1>
+
+//         <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
+//           <div className="md:w-1/2 ">
+//             <h1 className="text-center text-2xl font-bold mb-6 md:text-left">
+//               Get to know me!
+//             </h1>
+//             <p>
+//               Hi, my name is Syed Husain and I am a{" "}
+//               <span className="font-bold">{"highly ambitious"}</span>,
+//               <span className="font-bold">{" self-motivated"}</span>, and
+//               <span className="font-bold">{" driven"}</span> software engineer
+//               based in Los Angeles, CA.
+//             </p>
+//             <br />
+//             <p>
+//               I graduated from California State University, Northridge in 2019
+//               with a BS in Computer Engineering and have been working in the
+//               field ever since.
+//             </p>
+//             <br />
+//             <p>
+//               I have a wide range of hobbies and passions that keep me busy.
+//               From reading, playing sports, traveling, to making YouTube videos,
+//               I am always seeking new experiences and love to keep myself
+//               engaged and learning new things.
+//             </p>
+//             <br />
+//             <p>
+//               I believe that you should{" "}
+//               <span className="font-bold text-teal-500">
+//                 never stop growing
+//               </span>{" "}
+//               and that&#39;s what I strive to do, I have a passion for
+//               technology and a desire to always push the limits of what is
+//               possible. I am excited to see where my career takes me and am
+//               always open to new opportunities. 🙂
+//             </p>
+//           </div>
+//           <div className="text-center md:w-1/2 md:text-left">
+//             <h1 className="text-2xl font-bold mb-6">My Skills</h1>
+//             <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+//               {skills.map((item, idx) => {
+//                 return (
+//                   <p
+//                     key={idx}
+//                     className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+//                   >
+//                     {item.skill}
+//                   </p>
+//                 )
+//               })}
+//             </div>
+//             <Image
+//               src="/hero-image.png"
+//               alt=""
+//               width={325}
+//               height={325}
+//               className="hidden md:block md:relative md:bottom-4 md:left-32 md:z-0"
+//             />
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   )
+// }
+
+// export default AboutSection
+
+import Image from 'next/image'; // Import Image component if not already imported
 
 const AboutSection = () => {
+  const skills = [
+    { skill: 'Python' },
+    { skill: 'R' },
+    { skill: 'C++' },
+    { skill: 'C' },
+    { skill: 'Java' },
+    { skill: 'SQL' },
+    { skill: 'AWS' },
+    { skill: 'JavaScript' },
+    { skill: 'Node.js' },
+    { skill: 'Flask' },
+    { skill: 'Assembly' },
+    // Add more skills if needed
+  ];
+
   return (
     <section id="about">
       <div className="my-12 pb-12 md:pt-16 md:pb-48">
@@ -30,35 +123,23 @@ const AboutSection = () => {
               Get to know me!
             </h1>
             <p>
-              Hi, my name is Hosna and I am a{" "}
+              Hi, my name is Syed Husain and I am a{" "}
               <span className="font-bold">{"highly ambitious"}</span>,
               <span className="font-bold">{" self-motivated"}</span>, and
               <span className="font-bold">{" driven"}</span> software engineer
-              based in Los Angeles, CA.
+              based in Jessup, MD.
             </p>
             <br />
             <p>
-              I graduated from California State University, Northridge in 2019
-              with a BS in Computer Engineering and have been working in the
-              field ever since.
+              I am currently a Senior pursuing a Computer Science degree at UMBC, with a track in Data Science and AI/ML. I have substantial experience across programming languages including Python, C++, C, Java, JavaScript, Node.js, JavaFX, and x86 Assembly.
             </p>
             <br />
             <p>
-              I have a wide range of hobbies and passions that keep me busy.
-              From reading, playing sports, traveling, to making YouTube videos,
-              I am always seeking new experiences and love to keep myself
-              engaged and learning new things.
+              Proficient in data analysis, I have internship experience using FileNet, and I hold certifications in SQL, Machine Learning, DevOps, and AWS. Goal-driven with a talent for solving problems using a data-driven approach, I am known for my collaborative nature and strong work ethic.
             </p>
             <br />
             <p>
-              I believe that you should{" "}
-              <span className="font-bold text-teal-500">
-                never stop growing
-              </span>{" "}
-              and that&#39;s what I strive to do, I have a passion for
-              technology and a desire to always push the limits of what is
-              possible. I am excited to see where my career takes me and am
-              always open to new opportunities. 🙂
+              I am excited to see where my career takes me and am always open to new opportunities. 🙂
             </p>
           </div>
           <div className="text-center md:w-1/2 md:text-left">
@@ -75,13 +156,6 @@ const AboutSection = () => {
                 )
               })}
             </div>
-            <Image
-              src="/hero-image.png"
-              alt=""
-              width={325}
-              height={325}
-              className="hidden md:block md:relative md:bottom-4 md:left-32 md:z-0"
-            />
           </div>
         </div>
       </div>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,3 @@
-"use client" // this is a client component
 import React from "react"
 import { useState } from "react"
 import { Link } from "react-scroll/modules"
@@ -16,6 +15,10 @@ const NAV_ITEMS: Array<NavItem> = [
   {
     label: "Home",
     page: "home",
+  },
+  {
+    label: "Experience",
+    page: "experience",
   },
   {
     label: "About",
@@ -39,7 +42,7 @@ export default function Navbar() {
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
             <Link to="home">
               <div className="container flex items-center space-x-2">
-                <h2 className="text-2xl font-bold">Hosna Qasmei</h2>
+                <h2 className="text-2xl font-bold">Syed Husain</h2>
               </div>
             </Link>
             <div className="md:hidden">
