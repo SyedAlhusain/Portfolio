880634ca0850f76b4d2c5c55795e3d7d99818c78
SyedAlhusain <108646522+SyedAlhusain@users.noreply.github.com>
2024-02-23 22:27:39 -0500

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import { useState } from "react";
 import { useTheme } from "next-themes";
 import Link from 'next/link';
+import { usePathname } from "next/navigation"
+import { useTheme } from "next-themes"
+import { RiMoonFill, RiSunLine } from "react-icons/ri"
+import { IoMdMenu, IoMdClose } from "react-icons/io"
 
 interface NavItem {
   label: string;
