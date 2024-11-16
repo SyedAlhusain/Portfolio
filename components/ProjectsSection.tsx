117d3facadb37bb85c7391a0f9eed80c78acda00
Syed Alamdar Husain <108646522+SyedAlhusain@users.noreply.github.com>
2024-11-16 11:55:57 -0500

    Update ProjectsSection.tsx

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -6,6 +6,13 @@ import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs"
 
 const projects = [
   {
+    name: "Home Sales Price Trends Predictor",
+    description: "This project aim's in predicting the trends in average sales prices of US homes over time using various regression models including Linear Regression, Polynomial Regression, Ridge Regression, and Support Vector Regression (SVR).",
+    image: "/Housing_Predictor.png",
+    github: "https://github.com/SyedAlhusain/Predicting-US-Home-Sales-Price-Trends",
+    link: "https://syedahusain.pythonanywhere.com/",
+  },
+  {
     name: "Drift Travel App",
     description:
       "Drift is a travel app that allows user to share their trips/journeys, find new ones and create destination folders for saving and organizing favorite trips.",
@@ -14,27 +21,19 @@ const projects = [
     link: "https://github.com/SyedAlhusain/Drift-Travel-App",
   },
   {
-    name: "Stock Agent Chat Bot ",
-    description: "The Stock Trader Chat Bot uses OpenAI's GPT-3.5 model to deliver tailored stock trading insights in a more casual friendly way. It uses Flask for web routing with a user-friendly react frontend.",
-    image: "/Chat_Bot.webp",
-    github: "https://github.com/SyedAlhusain/Stock-Trader-Chat-Bot",
-    link: "https://syedhusain331.pythonanywhere.com/",
-  },
-  {
-    name: "Home Sales Price Trends Predictor",
-    description: "This project aim's in predicting the trends in average sales prices of US homes over time using various regression models including Linear Regression, Polynomial Regression, Ridge Regression, and Support Vector Regression (SVR).",
-    image: "/Housing_Predictor.png",
-    github: "https://github.com/SyedAlhusain/Predicting-US-Home-Sales-Price-Trends",
-    link: "https://syedahusain.pythonanywhere.com/",
-  },
-  {
     name: "S&P 500 Stock Predictor",
     description: "The S&P 500 Stock Predictor trains machine learning models to forecast stock price trends for companies listed on the S&P 500 index. The project utilized historical stock data from the year 2000 to 2020 for various tickers within the S&P 500. ",
     image: "/Stock_Predictor.avif",
     github: "https://github.com/SyedAlhusain/Stock-Price-Prediction-Machine-Learning-Models/tree/main",
     link: "https://github.com/SyedAlhusain/Stock-Price-Prediction-Machine-Learning-Models/tree/main",
   },
-  
+  {
+    name: "Stock Agent Chat Bot ",
+    description: "The Stock Trader Chat Bot uses OpenAI's GPT-3.5 model to deliver tailored stock trading insights in a more casual friendly way. It uses Flask for web routing with a user-friendly react frontend.",
+    image: "/Chat_Bot.webp",
+    github: "https://github.com/SyedAlhusain/Stock-Trader-Chat-Bot",
+    link: "https://syedhusain331.pythonanywhere.com/",
+  },  
     
 
 ]
